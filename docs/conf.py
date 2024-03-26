404ae88dca41b8e7e5781074307c11c0d0b58370
wohnlice <wohnliche@imsweb.com>
2024-03-26 16:03:46 -0400

    docs test

--- docs/conf.py
+++ docs/conf.py
@@ -1,16 +1,12 @@
-import os
-import sys
-
 from importlib.metadata import version as meta_version
 
 project = 'pub.tools'
-copyright = '2022, Eric Wohnlich'
+copyright = '2024, Eric Wohnlich'
 author = 'Eric Wohnlich'
 
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx_tabs.tabs',
-    'sphinxarg.ext'
 ]
 
 source_suffix = {
@@ -18,24 +14,6 @@ source_suffix = {
     '.txt': 'restructuredtext',
     '.md': 'markdown',
 }
-
-eggs = os.environ.get('BUILDOUT_EGGS', None)
-if eggs:
-    src_eggs = os.listdir(os.path.join(eggs, '..', 'src'))
-    for egg in os.listdir(eggs):
-        sys.path.insert(0, os.path.join(eggs, egg))
-    for egg in src_eggs:
-        sys.path.insert(0, os.path.join(eggs, '..', 'src', egg))
-
-imsplone_root = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-sys.path.insert(0, imsplone_root)
-
-venv = os.environ.get('VIRTUAL_ENV', None)
-if venv:
-    sys.path.insert(0, os.path.join(venv, 'Lib', 'site-packages'))
-
-# html_logo = 'images/plone.png'
-# html_favicon = 'images/favicon.ico'
 
 version = meta_version('pub.tools')
 

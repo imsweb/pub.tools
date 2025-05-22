bd2440f2b18b608ab4c77f927694c42e17286b81
wohnlice <wohnliche@imsweb.com>
2025-05-22 13:50:40 -0400

    wrong data type

--- pub/tools/orcid.py
+++ pub/tools/orcid.py
@@ -4,7 +4,7 @@ PUBLIC_API = "https://pub.orcid.org/v3.0/"
 
 
 def get_author(orcid: str, full: bool = False) -> dict:
-    response = requests.get(f"{PUBLIC_API}{orcid}", headers={"Accept": "application/json"}, timeout="2.0")
+    response = requests.get(f"{PUBLIC_API}{orcid}", headers={"Accept": "application/json"}, timeout=2.0)
     if response.status_code != 200:
         raise requests.exceptions.HTTPError(f"REST API returned: {response.status_code}")
 
